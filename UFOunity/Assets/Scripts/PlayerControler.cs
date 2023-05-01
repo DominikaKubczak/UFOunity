c319b3625a2895242d0f55990526e1b91de4e281
DominikaKubczak <93837935+DominikaKubczak@users.noreply.github.com>
2023-05-01 15:15:59 +0200

    Added UFO movement

--- UFOunity/Assets/Scripts/PlayerControler.cs
+++ UFOunity/Assets/Scripts/PlayerControler.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
+    Rigidbody2D rb2d;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * 15);
     }
 
     // Update is called once per frame
